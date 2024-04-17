3f0bc4de93e8e9e7a6c0e4cbb1f3c21645cdbf31
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-04-17 13:04:53 +0200

    Models underlying theoverview page

---
+++ GarageMVC/GarageMVC/ViewModels/VehicleOverviewViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GarageMVC.Models;
+
+namespace GarageMVC.ViewModels
+{
+    public class VehicleOverviewViewModel
+    {
+        public string? Type { get; }
+        public string? RegistrationNumber { get; }
+        public DateTime TimeStamp { get; }
+        public VehicleOverviewViewModel(ParkedVehicleModel parkedVehicle)
+        {
+            Type = parkedVehicle.Type;
+            RegistrationNumber = parkedVehicle.RegistrationNumber;
+            TimeStamp = parkedVehicle.TimeStamp;
+        }
+    }
+
+}
