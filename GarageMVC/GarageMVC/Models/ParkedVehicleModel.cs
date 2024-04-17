5b18122e2e12d83345b1b51408860aa520d09830
ulfbo <ulfbourelius71@gmail.com>
2024-04-17 10:44:57 +0200

    Added ParkedVehicleModel

---
+++ GarageMVC/GarageMVC/Models/ParkedVehicleModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GarageMVC.Models
+{
+    public class ParkedVehicleModel
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NumberOfWheels { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
