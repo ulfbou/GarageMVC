dda6a6a6f6dd62bb534c41e3b7d126bbd47a9182
rezapps <jus3p3@proton.me>
2024-04-17 08:50:19 +0000

    base project, added gitignore

--- GarageMVC/GarageMVC/Models/ParkedVehicleModel.cs
+++ GarageMVC/GarageMVC/Models/ParkedVehicleModel.cs
@@ -1,13 +1,16 @@
-﻿namespace GarageMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GarageMVC.Models
 {
     public class ParkedVehicleModel
     {
+        [Key]
         public int Id { get; set; }
-        public string Type { get; set; }
-        public string Color { get; set; }
-        public string RegistrationNumber { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
+        public string? Type { get; set; }
+        public string? Color { get; set; }
+        public string? RegistrationNumber { get; set; }
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
         public int NumberOfWheels { get; set; }
         public DateTime TimeStamp { get; set; }
     }
