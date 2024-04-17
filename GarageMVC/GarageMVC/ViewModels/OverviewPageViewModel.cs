64cba5463385ba7d8014552c5b999b310175dfcd
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-04-17 13:04:53 +0200

    Models underlying theoverview page

---
+++ GarageMVC/GarageMVC/ViewModels/OverviewPageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GarageMVC.ViewModels
+{
+    public class OverviewPageViewModel
+    {
+        public uint PlacesRemaining { get;}
+        public IEnumerable<VehicleOverviewViewModel> VehicleOverviews { get;}
+    }
+}
